0be17a4a49115689ca45bd365eea554f5105e935
QianYu-Du <sam6124883@gmail.com>
2025-06-23 09:01:14 +0800

    1. 修改主頁面: StatefulWidget 改用 ConsumerWidget 2. 加入下拉刷新功能

--- lib/main.dart
+++ lib/main.dart
@@ -4,52 +4,45 @@ import 'package:my_first_flutter/views/user_list_view/view/user_list_view.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyFirstFlutterApp()));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyFirstFlutterApp extends ConsumerWidget {
+  const MyFirstFlutterApp({super.key});
+
+  // 只有這邊使用，因此不需放到全域
+  static final selectedIndexProvider = StateProvider<int>((ref) => 0);
 
   @override
-  State<StatefulWidget> createState() => _MyAppState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedIndex = ref.watch(selectedIndexProvider);
 
-class _MyAppState extends State<MyApp> {
-  int _selectedIndex = 0;
+    final List<Widget> pages = [
+      const UserListView(), // 第一頁
+      const Center(child: Text('設定頁面')), // 第二頁
+    ];
 
-  final List<Widget> _pages = [
-    UserListView(), // 第一頁
-    Center(child: Text('設定頁面')), // 第二頁
-  ];
+    final List<String> titles = ["用戶列表", "設定"];
 
-  final List<String> _titles = ["用戶列表", "設定"];
-
-  // Bottom bar selected index
-  void onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My First MVVM Flutter Demo',
       theme: ThemeData(scaffoldBackgroundColor: themeBackgroundColor),
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            _titles[_selectedIndex],
+            titles[selectedIndex],
             style: TextStyle(color: barTextColor),
           ),
           backgroundColor: mainThemeColor,
           centerTitle: true,
         ),
-        body: _pages[_selectedIndex],
+        body: pages[selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: onItemTapped,
+          currentIndex: selectedIndex,
+          onTap: (index) {
+            // 更新狀態
+            ref.read(selectedIndexProvider.notifier).state = index;
+          },
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.list), label: "使用者"),
             BottomNavigationBarItem(icon: Icon(Icons.settings), label: "設定"),
@@ -63,3 +56,63 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+
+// StatefulWidget 寫法
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<StatefulWidget> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   int _selectedIndex = 0;
+
+//   final List<Widget> pages = [
+//     UserListView(), // 第一頁
+//     Center(child: Text('設定頁面')), // 第二頁
+//   ];
+
+//   final List<String> titles = ["用戶列表", "設定"];
+
+//   // Bottom bar selected index
+//   void onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'My First MVVM Flutter Demo',
+//       theme: ThemeData(scaffoldBackgroundColor: themeBackgroundColor),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(
+//             titles[_selectedIndex],
+//             style: TextStyle(color: barTextColor),
+//           ),
+//           backgroundColor: mainThemeColor,
+//           centerTitle: true,
+//         ),
+//         body: pages[_selectedIndex],
+//         bottomNavigationBar: BottomNavigationBar(
+//           currentIndex: _selectedIndex,
+//           onTap: onItemTapped,
+//           items: const [
+//             BottomNavigationBarItem(icon: Icon(Icons.list), label: "使用者"),
+//             BottomNavigationBarItem(icon: Icon(Icons.settings), label: "設定"),
+//           ],
+//           backgroundColor: mainThemeColor,
+//           selectedItemColor: selectedItemColor,
+//           unselectedItemColor: unselectedItemColor,
+//           selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/views/user_list_view/view/user_list_view.dart
+++ lib/views/user_list_view/view/user_list_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_first_flutter/global_assest/colors.dart';
 import 'package:my_first_flutter/views/user_list_view/view_model/user_list_view_model.dart';
 
 class UserListView extends ConsumerWidget {
-  UserListView({super.key});
+  const UserListView({super.key});
 
-  final userListProvider = FutureProvider((ref) {
+  static final userListProvider = FutureProvider((ref) {
     final vm = ref.read(userListViewModelProvider);
     return vm.fetchUsers();
   });
@@ -15,23 +16,38 @@ class UserListView extends ConsumerWidget {
     final userListAsync = ref.watch(userListProvider);
 
     return userListAsync.when(
-      data: (users) => ListView.separated(
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          final user = users[index];
-          return ListTile(
-            title: Text(
-              user.name,
-              style: TextStyle(fontSize: 24, color: Colors.black),
-            ),
-            subtitle: Text(
-              user.email,
-              style: TextStyle(fontSize: 18, color: Colors.black),
-            ),
-          );
+      data: (users) => RefreshIndicator(
+        color: mainThemeColor,
+        backgroundColor: Colors.white,
+        onRefresh: () async {
+          ref.invalidate(userListProvider);
+          await ref.read(userListProvider.future);
         },
-        separatorBuilder: (context, index) =>
-            const Divider(thickness: 1, height: 0.5, color: Colors.grey),
+        child: ListView.separated(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://i.pravatar.cc/150?u=${user.id}',
+                ),
+                radius: 24,
+              ),
+              title: Text(
+                user.name,
+                style: const TextStyle(fontSize: 24, color: Colors.black),
+              ),
+              subtitle: Text(
+                user.email,
+                style: const TextStyle(fontSize: 18, color: Colors.black),
+              ),
+              onTap: () => print("user: ${user.name}"),
+            );
+          },
+          separatorBuilder: (context, index) =>
+              const Divider(thickness: 1, height: 0.5, color: Colors.grey),
+        ),
       ),
       loading: () => const Center(child: CircularProgressIndicator()),
       error: (err, stack) => Center(child: Text('錯誤: $err')),
